d2103d07bcb09bd1eac149adcbf7d9fcc0984b09
Rohitpatil095 <rohitpatil1340sa@gmail.com>
2024-01-19 13:54:02 +0530

    navbar added

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,11 +1,13 @@
 import React from "react";
 import "./App.css";
+import { ExploreTopBooks } from "./components/HomePage/ExploreTopBooks";
 import { Navbar } from "./components/navbar/header";
 
 function App() {
   return (
     <>
       <Navbar />
+      <ExploreTopBooks/>
     </>
   );
 }
