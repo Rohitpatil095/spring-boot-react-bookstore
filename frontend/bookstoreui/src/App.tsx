adc271981570682527d6f44e9e7fb6c9e924a353
Rohitpatil095 <rohitpatil1340sa@gmail.com>
2024-05-31 23:05:41 +0530

    added constants file

--- frontend/bookstoreui/src/App.tsx
+++ frontend/bookstoreui/src/App.tsx
@@ -1,14 +1,37 @@
-import './App.css';
-import { Footer } from './layouts/Homepage/Footer';
-import { HomePage } from './layouts/Homepage/HomePage';
-import { Navbar } from './layouts/NavbarAndFooter/Navbar';
+import { Redirect, Route, Switch } from "react-router-dom";
+import "./App.css";
+import { ExploreTopBooks } from "./layouts/Homepage/ExploreTopBooks";
+import { Footer } from "./layouts/NavbarAndFooter/Footer";
+import { HomePage } from "./layouts/Homepage/HomePage";
+import { Navbar } from "./layouts/NavbarAndFooter/Navbar";
+import { SearchBooks } from "./layouts/SearchBooksPage/SearchBooks";
+import { BookCheckoutPage } from "./layouts/CheckoutPage/BookCheckoutPage";
 
 function App() {
   return (
-    <div className="App">
-      <Navbar/>
-      <HomePage/>
-      <Footer/>
+    <div className="d-flex flex-column min-vh-100">
+      <Navbar />
+      <div className="flex-grow-1">
+        <Switch>
+          <Route path="/" exact>
+            <Redirect to="/home"></Redirect>
+          </Route>
+
+          <Route path="/home">
+            <HomePage />
+          </Route>
+
+          <Route path="/Search">
+            <SearchBooks />
+          </Route>
+
+          <Route path="/checkout/:bookId">
+            <BookCheckoutPage/>
+          </Route>
+
+        </Switch>
+      </div>
+      <Footer />
     </div>
   );
 }
--- frontend/bookstoreui/src/constants/serviceConstants.ts
+++ frontend/bookstoreui/src/constants/serviceConstants.ts
@@ -1,4 +1,10 @@
-export const BACKEND_BASE_SERVICE_URL: string = "http://localhost:8080/api/books";
-export const BACKEND_SERVICE_URL:string =BACKEND_BASE_SERVICE_URL+"/api/books";
-// export const FETCH_ALL_BOOKS: string=BACKEND_SERVICE_URL+"";
-export const FETCH_ALL_BOOKS: string = "http://localhost:8080/api/books?page=0&size=9";
+export const BACKEND_BASE_SERVICE_URL: string = "http://localhost:8080";
+export const BACKEND_BOOKS_SERVICE_URL:string =BACKEND_BASE_SERVICE_URL+"/api/books";
+export const PAGE_SIZE: Number=9;
+export const PAGE_NUMBER: Number=0;
+export const FETCH_ALL_BOOKS: string = BACKEND_BOOKS_SERVICE_URL + "? page="+PAGE_NUMBER+"&size="+PAGE_SIZE;
+export const GET_BOOK_WITH_ID: string= BACKEND_BOOKS_SERVICE_URL;
+
+// export const FETCH_ALL_BOOKS: string = "http://localhost:8080/api/books?page=0&size=9";
+
+
